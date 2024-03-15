8e71ca3c08d22f0c1e1321bba6acac9c05cb8579
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-03-15 18:19:40 +1100

    Create function to define which operation to execute

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ function divide(dividend, divisor) {
 
     return dividend / divisor;
 }
+
+
+function operate(operator, num1, num2) {
+// Function to define which operation to execute
+
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        
+        case '-':
+            return substract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+        
+        case '/':
+            return divide(num1, num2);
+        
+        default:
+            return ERROR;
+    }
+}
