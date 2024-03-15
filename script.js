9035098801ab870b52392988c42962dca0aadf64
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-03-15 18:12:53 +1100

    Create multiply and divide functions

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function substract(num1, num2) {
 
     return num1 - num2;
 }
+
+function multiply (num1, num2) {
+// Basic multiplication function
+
+    return num1 * num2;
+}
+
+
+function divide(dividend, divisor) {
+// Basic division function
+
+    return dividend / divisor;
+}
