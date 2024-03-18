fb9184edf7f3e8667e148bd5378c8a1880ae9340
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-03-18 22:06:57 +1100

    Create functions to record operator and numbers iput

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// 
+// Project variables
+// 
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+let results = "";
+
+// 
+// Functions
+//
+
 function add(num1, num2) {
 // Basic sum function
 
@@ -45,3 +57,60 @@ function operate(operator, num1, num2) {
             return ERROR;
     }
 }
+
+function recordNumber(input) {
+// Function to save numbers entered by the user in a variable 
+
+    currentNumber = currentNumber + input;
+};
+
+function recordOperator(input) {
+// Function to save the operator entered by the user in a variable
+
+    operator = input;
+}
+
+function getInput(event) {
+// Function to record buttons pressed by the user and store them in a correct variable
+
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let operations = ["+", "-", "*", "/"];
+let input = event.target.value;
+
+if (numbers.includes(input)) {
+    recordNumber(input);
+} 
+else if (operations.includes(input)) {
+    if (previousNumber == "") {
+
+        // Move the recorded number into the previousNumber variable so it can be used later to operate
+        previousNumber = currentNumber;
+        currentNumber = "";
+        recordOperator(input);
+    }
+    else {
+        let results = operate(operator, +previousNumber, +currentNumber);
+        previousNumber = results;
+        currentNumber = "";
+        recordOperator(input);
+    }
+}
+
+console.log(`Current number = ${currentNumber}`);
+console.log(`Previous number = ${previousNumber}`);
+console.log(`Operator = ${operator}`);
+console.log(`Results = ${results}`);
+}
+
+
+// 
+// Event listeners
+// 
+buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        getInput(event);
+    }); 
+});
+
